f6ae72c1f5de01f4d27ce4db23ae8a6947657d12
emjayMJkim <minjeong030420@gmail.com>
2023-09-05 02:18:07 +0900

    [ADD] 4659 파일 생성

---
+++ 자료구조/민정/4659.cpp
@@ -0,0 +1,27 @@
+// ----------------
+// 4659 비밀번호 발음하기
+// 
+// minjeong
+// ----------------
+
+#include <iostream>
+
+using namespace std;
+
+char vowel[5] = {'a', 'e', 'i', 'o', 'u'};
+
+void rule1() {
+
+}
+
+void rule2() {
+
+}
+
+void rule3() {
+    
+}
+
+int main(void) {
+
+}
