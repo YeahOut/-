2a8196802628151385b79d5ee8347c4600222ab8
emjayMJkim <minjeong030420@gmail.com>
2023-09-11 19:22:44 +0900

    [ADD] 조건 주석 추가

--- 자료구조/민정/4659.cpp
+++ 자료구조/민정/4659.cpp
@@ -10,16 +10,19 @@ using namespace std;
 
 char vowel[5] = {'a', 'e', 'i', 'o', 'u'};
 
+// 조건 1: 모음 하나를 반드시 포함
 void rule1() {
 
 }
 
+// 조건 2: 모음 3개 혹은 자음이 3개 연속으로 오면 안 됨
 void rule2() {
 
 }
 
+// 조건 3: 같은 글자 연속 안 됨 (단, ee와 oo는 허용)
 void rule3() {
-    
+
 }
 
 int main(void) {
