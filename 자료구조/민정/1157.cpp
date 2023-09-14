d810f1370671edaa0940b198c5dc6ab82bc7d566
emjayMJkim <minjeong030420@gmail.com>
2023-09-15 03:48:41 +0900

    [FEAT] 1157 단어 공부 구현

--- 자료구조/민정/1157.cpp
+++ 자료구조/민정/1157.cpp
@@ -5,21 +5,31 @@
 // ----------------
 
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 void checkMax(string s) {
     int use_num[26] = {};
-    int s_index;
+    int use_index, max_index;
+    char max_word;
 
     for (int i = 0; i < s.length(); i++) {
-        s_index = s[i] - 65;
-        use_num[s_index]++;
+        use_index = s[i] - 65;
+        use_num[use_index]++;
     }
 
-    for (int i=0; i<26; i++) {
-        cout << use_num[i] << " / ";
+    max_index = max_element(begin(use_num), end(use_num)) - begin(use_num);
+    max_word = max_index + 65;
+
+    // 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ? 출력
+    for (int i = 0; i < 26; i++) {
+        if (i == max_index) continue;
+        if (use_num[i] == use_num[max_index]) max_word = '?';
     }
+
+    cout << max_word;
+    
 }
 
 int main(void) {
@@ -27,7 +37,6 @@ int main(void) {
 
     cin >> cmd;
 
-    cout << endl;
     for (int i = 0; i < cmd.length(); i++) {
         cmd[i] = toupper(cmd[i]);
     }
