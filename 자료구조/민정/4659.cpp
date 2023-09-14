bd017d3427ca403dd4af066305f2fca5d36b080a
emjayMJkim <minjeong030420@gmail.com>
2023-09-15 02:08:30 +0900

    [FEAT] 4659 비밀번호 발음하기 구현

--- 자료구조/민정/4659.cpp
+++ 자료구조/민정/4659.cpp
@@ -20,9 +20,26 @@ bool rule1(string s) {
     return false;
 }
 
-// 조건 2: 모음 3개 혹은 자음이 3개 연속으로 오면 안 됨
-void rule2() {
+bool chechar(char c) {
+    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            return true;
+        }
+    return false;
+}
 
+// 조건 2: 모음 3개 혹은 자음이 3개 연속으로 오면 안 됨
+bool rule2(string s) {
+    if (s.length() < 2) return true;
+
+    for (int i = 0; i < s.length() - 2; i++) {
+        if (chechar(s[i]) && chechar(s[i + 1]) && chechar(s[i + 2])) {
+            return false;
+        }
+        else if (!chechar(s[i]) && !chechar(s[i + 1]) && !chechar(s[i + 2])) {
+            return false;
+        }
+    }
+    return true;
 }
 
 // 조건 3: 같은 글자 연속 안 됨 (단, ee와 oo는 허용)
@@ -44,7 +61,11 @@ int main(void) {
 
         if (cmd == "end") break;
 
-        if (rule3(cmd)) cout << cmd << ": " << rule3(cmd) << endl;   
+        if (rule1(cmd) && rule2(cmd) && rule3(cmd)) {
+            cout << "<" << cmd << ">" << " is acceptable." << endl;
+        } else {
+            cout << "<" << cmd << ">" << " is not acceptable." << endl;
+        }
         
     }
 }
