76d6b7eb2b9b744da4dae219517b81ad72fd9932
emjayMJkim <minjeong030420@gmail.com>
2023-09-14 21:53:39 +0900

    [ADD] 조건 1 구현

--- 자료구조/민정/4659.cpp
+++ 자료구조/민정/4659.cpp
@@ -11,8 +11,13 @@ using namespace std;
 char vowel[5] = {'a', 'e', 'i', 'o', 'u'};
 
 // 조건 1: 모음 하나를 반드시 포함
-void rule1() {
-
+bool rule1(string s) {
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+            return true;
+        }
+    }
+    return false;
 }
 
 // 조건 2: 모음 3개 혹은 자음이 3개 연속으로 오면 안 됨
@@ -26,5 +31,15 @@ void rule3() {
 }
 
 int main(void) {
+    string cmd;
 
+    while (1) {
+
+        cin >> cmd;
+
+        if (cmd == "end") break;
+
+        if (rule1(cmd)) cout << cmd << ": " << rule1(cmd) << endl;   
+        
+    }
 }
