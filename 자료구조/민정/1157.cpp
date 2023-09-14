2f59d5368b00aaef8a801a2b9245a4f6043d9f81
emjayMJkim <minjeong030420@gmail.com>
2023-09-15 03:00:19 +0900

    [ADD] 알파벳 사용 수 계산 구현

--- 자료구조/민정/1157.cpp
+++ 자료구조/민정/1157.cpp
@@ -8,6 +8,30 @@
 
 using namespace std;
 
+void checkMax(string s) {
+    int use_num[26] = {};
+    int s_index;
+
+    for (int i = 0; i < s.length(); i++) {
+        s_index = s[i] - 65;
+        use_num[s_index]++;
+    }
+
+    for (int i=0; i<26; i++) {
+        cout << use_num[i] << " / ";
+    }
+}
+
 int main(void) {
+    string cmd;
+
+    cin >> cmd;
+
+    cout << endl;
+    for (int i = 0; i < cmd.length(); i++) {
+        cmd[i] = toupper(cmd[i]);
+    }
+
+    checkMax(cmd);
     
 }
