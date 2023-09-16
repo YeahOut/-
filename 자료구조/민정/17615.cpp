32aabee7815eaa6cecab9372190ae47b5818b4e8
emjayMJkim <minjeong030420@gmail.com>
2023-09-16 16:12:45 +0900

    [FIX] 백준 에러 해결

--- 자료구조/민정/17615.cpp
+++ 자료구조/민정/17615.cpp
@@ -8,7 +8,6 @@
 
 using namespace std;
 
-int ball_num;
 
 void changeBall(int index_origin, int index_change, char array[], int& num) {
     char temp;
@@ -19,10 +18,10 @@ void changeBall(int index_origin, int index_change, char array[], int& num) {
     num++;
 }
 
-void searchDifferentBall(char array[], char right, int& num) {
+void searchDifferentBall(char array[], char right, int& num, int start_index) {
     int origin_index, change_index;
 
-    for (int i = ball_num - 2; i >= 0; i--) {
+    for (int i = start_index; i >= 0; i--) {
         if (array[i] != right && array[i + 1] == right) {
                 change_index = i;
             }
@@ -37,20 +36,40 @@ void searchDifferentBall(char array[], char right, int& num) {
 }
 
 int main(void) {
+    int ball_num, ball_num_2;
     string cmd;
-    int change_num = 0;
+    int change_num = 0, change_num_2 = 0;
+    int min, second_ball_index;
 
     cin >> ball_num;
     cin >> cmd;
 
-    char ball_array[ball_num];
-    char rightMostBall;
+    char ball_array[ball_num], ball_array2[ball_num];
+    char right_ball_1, right_ball_2;
     for (int i = 0; i < ball_num; i++) {
         ball_array[i] = cmd[i];
+        ball_array2[i] = cmd[i];
     }
-    rightMostBall = ball_array[ball_num - 1];
+    right_ball_1 = ball_array[ball_num - 1];
 
-    searchDifferentBall(ball_array, rightMostBall, change_num);
+    searchDifferentBall(ball_array, right_ball_1, change_num, ball_num - 2);
 
-    cout << change_num;
+
+    min = change_num;
+
+    change_num = 0;
+    for (int i = ball_num - 1; i >= 0; i--) {
+        if (ball_array2[i] != right_ball_1) {
+
+            changeBall(i, ball_num - 1, ball_array2, change_num);
+            right_ball_2 = ball_array2[ball_num - 1];
+            break;
+        }
+    }
+
+    searchDifferentBall(ball_array2, right_ball_2, change_num, ball_num - 2);
+
+    min = (min < change_num) ? min : change_num;
+
+    cout << min;
 }
