ccc365b65dc49ce0e1c41248dae19e9a94f60763
emjayMJkim <minjeong030420@gmail.com>
2023-09-16 14:09:59 +0900

    [ADD] 가장 오른쪽 볼 찾기

--- 자료구조/민정/17615.cpp
+++ 자료구조/민정/17615.cpp
@@ -8,6 +8,29 @@
 
 using namespace std;
 
+void searchDifferentBall() {
+
+}
+
+void changeBall() {
+
+}
+
 int main(void) {
+    int ball_num;
+    string cmd;
     
+
+    cin >> ball_num;
+    cin >> cmd;
+
+    char ball_array[ball_num];
+    char rightMostBall;
+    for (int i = 0; i < ball_num; i++) {
+        ball_array[i] = cmd[i];
+    }
+    rightMostBall = ball_array[ball_num - 1];
+
+    // cout << endl;
+    // cout << "ri: " << rightMostBall;
 }
