36462ec081db9cb4bea0b00961c38fe7b4271eca
emjayMJkim <minjeong030420@gmail.com>
2023-09-17 01:46:33 +0900

    [FEAT] 10952 A+B-5 구현

---
+++ 자료구조/민정/10952.cpp
@@ -0,0 +1,20 @@
+// ----------------
+// 10952 A + B - 5
+// 
+// minjeong
+// ----------------
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int A, B;
+
+    while (1) {
+        cin >> A >> B;
+        if (A == 0 && B == 0) break;
+        else cout << A + B << endl;
+    }
+    
+}
